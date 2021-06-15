2f51c76acd784fcf3e67b9f5c2811838133d8dd5
Jesus <jesusadrian1953.1@gmail.com>
2021-06-14 22:38:35 -0500

    deleted build files

--- build/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-if (process.env.NODE_ENV === 'development') {
-}
-else if (process.env.NODE_ENV === 'ci') {
-}
-else {
-    //keys for production
-}
--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var PORT = Number(process.env.PORT) || 3000;
-app_1.app.listen(3000, function () {
-    console.log('Server up and running on port', PORT);
-});
